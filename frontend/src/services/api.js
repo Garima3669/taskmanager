17e68ad4588f76622dbb78c931159836631135cc
Garima Singla <garima1562.be22@chitkara.edu.in>
2026-06-27 22:41:43 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://taskmanager-backend-v6pu.onrender.com/api",
+  withCredentials: true,
 });
 
 // Automatically attach JWT Token
